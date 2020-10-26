4c593f6cd1abe5cb0fbafa325fd671e7d1aa464b
bederbourahmah <bederbourahmah@gmail.com>
2020-10-26 10:30:57 -0700

    Change to Vue 2.

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <b-navbar toggleable="lg" type="dark" variant="dark">
+        <b-navbar-brand to="/">
+          Legend Arena
+        </b-navbar-brand>
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <b-nav-item to="/">
+              Home
+            </b-nav-item>
+            <b-nav-item to="/about">
+              About
+            </b-nav-item>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  border-bottom-color: #ce3304;
+  border-bottom-width: 2px;
+  border-bottom-style: solid;
+
+  a {
+    font-weight: bold;
+    color: #ce3304;
+
+    &.router-link-exact-active {
+      color: #ff0000;
+    }
+  }
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <Login msg="Login" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Login from "@/components/Login.vue"; // @ is an alias to /src
+
+export default Vue.extend({
+  name: "Home",
+  components: {
+    Login
+  }
+});
+</script>
+
+<style lang="scss">
+.home {
+  background-color: #343a40;
+}
+</style>
